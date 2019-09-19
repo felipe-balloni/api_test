1ddcbc5eb69a380b87fa75e01343483517085c2d
Felipe Balloni Ferreira <felipe.balloni@fbftecnologia.com.br>
2019-09-19 14:40:04 -0300

    Validation init

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Product;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest as Request;
 
 class ProductController extends Controller
 {
